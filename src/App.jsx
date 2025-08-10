c31cd386f99d6661a34fed24691cbcddcd6bc4d7
lilhalzy <lilhalzy11@gmail.com>
2025-08-11 07:50:22 +0800

    initial commit
    Setup React + Vite + Tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+      <div className='text-3xl font-bold text-gray-400'>Ruang. is currently building.</div>
+    </div>
+  )
+}
+
+export default App
